5efe6311fc5f9a600bff8d56ea8a492e9bb163b7
bestwoody <bestwoody@163.com>
2022-10-18 15:32:52 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+cd /tiflash/monitor-9100/scripts
+./supervisor_blackbox_exp.sh >supervisor_blackbox_exp.log 2>&1 & 
+./supervisor_node_exp.sh >supervisor_node_exp.log 2>&1 & 
+cd /tiflash
+exec /tiflash/rpc_server
+
