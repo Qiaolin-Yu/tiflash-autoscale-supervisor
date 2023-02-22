7532b17f46f989696e6f6443453c2e72dcccc32c
Qiaolin Yu <liin1211@outlook.com>
2023-02-22 14:21:52 +0800

    change order

--- service_test.go
+++ service_test.go
@@ -8,6 +8,19 @@ import (
 	"testing"
 )
 
+func TestGetTiFlashTaskNumByMetricsByte(t *testing.T) {
+	data1, err := os.ReadFile("test_data/metrics1.txt")
+	assert.NoError(t, err)
+	res, err := GetTiFlashTaskNumByMetricsByte(data1)
+	assert.NoError(t, err)
+	assert.Equal(t, res, 0)
+	data2, err := os.ReadFile("test_data/metrics2.txt")
+	assert.NoError(t, err)
+	res, err = GetTiFlashTaskNumByMetricsByte(data2)
+	assert.NoError(t, err)
+	assert.Equal(t, res, 66)
+}
+
 func TestGetTiFlashTaskNumHttp(t *testing.T) {
 	data, err := os.ReadFile("test_data/metrics2.txt")
 	assert.NoError(t, err)
@@ -21,19 +34,6 @@ func TestGetTiFlashTaskNumHttp(t *testing.T) {
 
 	defer server.Close()
 	res, err := GetTiFlashTaskNum()
-	assert.NoError(t, err)
-	assert.Equal(t, res, 66)
-}
-
-func TestGetTiFlashTaskNumByMetricsByte(t *testing.T) {
-	data1, err := os.ReadFile("test_data/metrics1.txt")
-	assert.NoError(t, err)
-	res, err := GetTiFlashTaskNumByMetricsByte(data1)
-	assert.NoError(t, err)
-	assert.Equal(t, res, 0)
-	data2, err := os.ReadFile("test_data/metrics2.txt")
-	assert.NoError(t, err)
-	res, err = GetTiFlashTaskNumByMetricsByte(data2)
 	assert.NoError(t, err)
 	assert.Equal(t, res, 66)
 }
